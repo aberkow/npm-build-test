3e0e361bf70da8ea1df24b3868f00fbda4f0dded
aberkow <berkowitz.clarinet@gmail.com>
2016-05-03 21:08:46 -0400

    added npm task for uglify-js

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,14 @@
 var path = require('path');
 var webpack = require('webpack');
 var packageData = require('./package.json');
+var minify = process.argv.indexOf('--minify') != -1;
 var filename = [packageData.name, packageData.version, 'js'];
+var plugins = [];
 
+if (minify){
+  filename.splice(filename.length - 1, 0, 'min');
+  plugins.push(new webpack.optimize.UglifyJSPlugin());
+}
 module.exports = {
   entry: path.resolve(__dirname, packageData.main),
   output: {
@@ -21,5 +27,6 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: plugins
 }
